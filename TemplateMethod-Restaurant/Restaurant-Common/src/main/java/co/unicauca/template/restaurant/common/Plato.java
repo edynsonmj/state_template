e8feaeee9dc91327993cc58c7b3fa28e59ecba08
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2020-11-09 20:24:24 -0500

    subir

---
+++ TemplateMethod-Restaurant/Restaurant-Common/src/main/java/co/unicauca/template/restaurant/common/Plato.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.template.restaurant.common;
+
+/**
+ *
+ * @author EdynsonMJ
+ */
+public class Plato {
+    private int id;
+    private String nombre;
+    private String Descripcion;
+    public Plato(){}
+    public Plato(int id, String nombre, String descripcion){
+        this.Descripcion = descripcion;
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+    
+}
